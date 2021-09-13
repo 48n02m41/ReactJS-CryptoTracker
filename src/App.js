da3a9358eca896937dae3238a89d2e946cd15bec
48n02m41 <54042201+48n02m41@users.noreply.github.com>
2021-09-13 21:30:23 +0100

    Axios setup.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+  const [cryptocoins, setCryptoCoins] = useState([]);
+
+  useEffect(() => {
+
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=90&page=1&sparkline=false')
+    .then(res => {
+      setCryptoCoins(res.data);
+      //console.log(res.data)
+    })
+    .catch(error => console.log(error));
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stuff</h1>
     </div>
   );
 }
